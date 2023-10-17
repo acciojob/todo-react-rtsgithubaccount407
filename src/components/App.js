d4660ed24196aa3b0c1cce1f55060f9577da7557
Rohit <rs4787690@gmail.com>
2023-10-17 16:59:13 +0530

    submitting code-Tue Oct 17 2023 16:59:10 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  function implementAddToDo(e) {
+    e.preventDefault();
+    setTodoList([...todoList, todo])
+    setTodo("")
+  }
+
+  function handleDelete(index) {
+
+    setTodoList(todoList.filter((value,position)=>position !== index))
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={implementAddToDo}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todoList.map((item,index) => {
+          return (
+            <li key={item}>
+              {item}
+              <button onClick={()=>handleDelete(index)}>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
